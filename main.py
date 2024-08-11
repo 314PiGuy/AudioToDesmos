f17491eb1763bd1bc01ff64ef13cbc004f8dc089
314PiGuy <147645262+314PiGuy@users.noreply.github.com>
2024-08-11 09:51:43 -0400

    changed the algorithm to get only the one most dominant frequency and wrote code to output the dominant frequencies over time to a txt in desmos format

--- main.py
+++ main.py
@@ -3,47 +3,59 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.fft import fft
 import wave
-
-def dominantFreq(signal):
-  #fourier transformation to get dominant frequencies
-  signalfft = fft(signal)
-  amplitudeSpectrum = np.abs(signalfft)
-  amplitudeSpectrum = amplitudeSpectrum / np.max(amplitudeSpectrum)
-  freqs = np.fft.fftfreq(samples, 1 / sampleRate)
-
-  dominantFreqs = []
-  threshold = 0.1
-  #get the 3 most dominant frequencies
-  while 1:
-    dominantIndices = np.where(amplitudeSpectrum[:samples // 2] >= threshold)[0]
-    dominantFreqs = freqs[dominantIndices]
-    if (len(dominantFreqs) <= 3):
-      break
-    threshold += 0.01
-  return dominantFreqs
-
-def IntervalFreqs(signal, interval):
-  freqs = []
-  start = 0
-  while (start + interval <= len(signal)):
-    section = signal[start:start+interval]
-    freqs.append(dominantFreq(section))
-    start += interval
-  return freqs
+import os
 
 #get some audio info
 wav = wave.open('rick.wav', 'rb')
 sampleRate = wav.getframerate()
 samples = wav.getnframes()
-duration = samples/sampleRate
+duration = samples / sampleRate
+
 #get an array of the audio amplitude at each sample (includes all channels)
-signal = wav.readframes(samples)
-signalArray = np.frombuffer(signal, dtype=np.int16)
+signalRaw = wav.readframes(samples)
+signalArray = np.frombuffer(signalRaw, dtype=np.int16)
 
-lchannel = signalArray[1::2] #the amplitude array for only the left channel
+signal = signalArray[1::2]  #the amplitude array for only one channel
 
-interval = int(sampleRate/10) #get the dominant frequencies every 0.1 seconds
+intervalTime = 0.1
+
+interval = int(sampleRate * intervalTime)
+
+def dominantFreq(signal):
+  global interval
+  #fourier transformation to get dominant frequencies
+  signalfft = fft(signal)
+  amplitudeSpectrum = np.abs(signalfft)
+  if len(amplitudeSpectrum) == 0:
+    return 0
+  amplitudeSpectrum = amplitudeSpectrum / np.max(amplitudeSpectrum)
+  freqs = np.fft.fftfreq(interval, 1 / sampleRate)
+  return freqs[amplitudeSpectrum.argmax(axis=0)]
+
+def IntervalFreqs():
+  global signal, interval
+  freqs = []
+  start = 0
+  while (start + interval <= len(signal)):
+    section = signal[start:start + interval]
+    freqs.append(dominantFreq(section))
+    start += interval
+  return freqs
 
 
+freqs = IntervalFreqs()
 
-freqs = IntervalFreqs(lchannel, interval)
+def writeLine(data, start):
+  count = 0
+  with open('output.txt', 'a') as f:
+    f.write(chr(count+65)+'\\left(x\\right)=\\left\\{')
+    for time, freq in enumerate(data):
+      f.write(f'{round(time*intervalTime+start, 2)}<x\\le{round((time+1)*intervalTime+start, 2)}:{round(freq, 2)}, \\ ')
+  with open('output.txt', 'rb+') as f:
+    f.seek(-4, os.SEEK_END)
+    f.truncate()
+  with open('output.txt', 'a') as f:
+    f.write('\\right\\}\n')
+  count+=1
+
+writeLine(freqs, 0)
